bdff5642f585fee22174233c047d85cf96a51872
mbbenitez <mbbenitez@users.noreply.github.com>
2015-04-23 13:35:33 -0300

    Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+//Maria Belen Benitez Insaurralde
+//Trabajo Practico n°2 - Pares
+//23-04-2015
+
+#include <iostream>
+
+using namespace std;
+int main() {
+int n;
+
+		cout<< "Ingrese un numero entero: ";
+		cin >> n;
+
+		if ((n%2)==0){
+			cout<<"El numero ingresado es par";
+       }else{
+			cout<<"el numero es impar";
+		}
+	return 0;
+}
+
